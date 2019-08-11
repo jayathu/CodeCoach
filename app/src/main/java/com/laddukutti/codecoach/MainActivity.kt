77193890743ca715a78630f40f897010e31ace3b
jayashree nagarajan <jayathu@gmail.com>
2019-08-10 22:45:28 -0700

    added fragments

--- app/src/main/java/com/laddukutti/codecoach/MainActivity.kt
+++ app/src/main/java/com/laddukutti/codecoach/MainActivity.kt
@@ -1,32 +1,50 @@
 package com.laddukutti.codecoach
 
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.TextView
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
 
-    //private lateinit var textMessage: TextView
+    private lateinit var selectedFragment: Fragment
+
     private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
+
             R.id.navigation_home -> {
-                //textMessage.setText(R.string.title_home)
+
+                Log.d("DEBUG-TEST", "Home Fragment")
+                selectedFragment = HomeFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.frame_container, selectedFragment).commit()
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_calendar -> {
-                //textMessage.setText(R.string.title_dashboard)
+
+                Log.d("DEBUG-TEST", "Calendar Fragment")
+                selectedFragment = CalendarFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.frame_container, selectedFragment).commit()
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_tracker -> {
-                //textMessage.setText(R.string.title_notifications)
+
+                Log.d("DEBUG-TEST", "Tracker Fragment")
+                selectedFragment = TrackerFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.frame_container, selectedFragment).commit()
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_questionbank -> {
-                //textMessage.setText(R.string.title_notifications)
+
+                Log.d("DEBUG-TEST", "Question Bank Fragment")
+                selectedFragment = QuestionBankFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.frame_container, selectedFragment).commit()
                 return@OnNavigationItemSelectedListener true
             }
         }
+
+
         false
     }
 
